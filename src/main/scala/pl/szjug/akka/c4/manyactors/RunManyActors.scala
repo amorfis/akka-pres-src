81a90f09df34a33e26960b4752da56447e97a6aa
Paweł Stawicki <pawelstawicki@gmail.com>
2015-07-09 11:04:48 +0200

    Big fractal, many small jobs.

--- src/main/scala/pl/szjug/akka/c4/manyactors/RunManyActors.scala
+++ src/main/scala/pl/szjug/akka/c4/manyactors/RunManyActors.scala
@@ -1,19 +1,19 @@
 package pl.szjug.akka.c4.manyactors
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorSystem, Props}
 import com.mkrcah.fractals._
 import com.typesafe.scalalogging.LazyLogging
-import pl.szjug.fractals.Job
-import pl.szjug.akka.actors.ActorRenderer
 import pl.szjug.akka.Constants._
+import pl.szjug.akka.actors.ActorRenderer
+import pl.szjug.fractals.JobToDivide
 
 object RunManyActors extends App with LazyLogging {
-
+  private val size = Size2i(5000, 5000)
   val system = ActorSystem("actorSystem")
 
-  val workers = for (i <- 1 to 30) yield system.actorOf(Props[ActorRenderer])
+  val workers = for (i <- 1 to 10) yield system.actorOf(Props[ActorRenderer])
 
-  val master = system.actorOf(Props(classOf[ManyActorsMaster], imageSize, workers), "master")
+  val master = system.actorOf(Props(classOf[ManyActorsMaster], size, workers), "master")
 
-  master ! Job(imageSize, Region2i(imageSize), palette)
+  master ! JobToDivide(size, 100, 100, palette)
 }
